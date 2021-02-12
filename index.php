9d1d7a54f722e804dd4d499d2d9b2a3f04c8b1a5
Beata Rynkevič <beata.rynkevic1@gmail.com>
2021-02-12 16:42:03 +0200

    5 uzduotys

--- index.php
+++ index.php
@@ -1,39 +1,4 @@
-<?php
-
-$animals = ['Antelope' => 'Snorts',
-'Bat' => 'Screeches',
-'Bear' => 'Roar',
-'Dolphin' => 'Clicks',
-'Goose' => 'Honks',
-'Vulture' => 'Screams'];
-
-foreach ($animals as $name => $sound) {
-    // echo $name. ' '.$sound.'<br>';
-    echo $animals[$name].'<br />';
-}
-
-echo '<span style="color:pink;font-size: 40px;">-------------</span>' . '<br>';
-
-$numbers = [1, 2, 3, 4, 5];
-print_r($numbers);
-foreach ($numbers as &$number) {
-    $number = $number *2;
-}
-echo '<br>';
-print_r($numbers);
-?>
-
-
-<?php
-echo '<br>';
-$colors = array("red", "green", "blue", "yellow");
-
-foreach ($colors as $value) {
-  echo "$value <br>";
-}
-?>
-
-<h2 style="color:red";>---------1 Uzduotis---------</h2>
+<h2 style="color:#EFA00B";>---------1 Uzduotis---------</h2>
 
 <?php
 $visoSimboliu = str_repeat('*', 400);
@@ -46,8 +11,91 @@ for ($i = 0; $i < $eiluteje; $i++) {
     echo '<br>';
 }
 echo '<br><br>';
+?>
+
+<h2 style="color:#0267C1";>---------2 Uzduotis---------</h2>
+<?php
+
+$kiekSkaiciu = 300;
+$count = 0;
+
+for ($i = 0; $i <= $kiekSkaiciu; $i++) {
+    $skaicius = rand(0, 300);
+    if ($skaicius > 150) {
+        $count++;
+    }
+    if ($skaicius > 275) {
+        echo "<span style='color:red';> $skaicius </span>";
+    } else {
+        echo $skaicius . ' ';
+    }
+}
+?>
 
 
 
+<h2 style="color:#0075C4";>---------3 Uzduotis---------</h2>
+<?php
 
+$atsitiktinisSkaicius = rand(3000, 4000);
+$stringas= " ";
+echo 'atsitiktinis skaicius yra: ' . $atsitiktinisSkaicius . '<br>';
+for ($i = 1; $i <= $atsitiktinisSkaicius; $i ++) {
+    if ($i % 77 === 0) {
+        $stringas .= ($i . ", ");
+    }
+}
+echo substr($stringas, 0, (mb_strlen($stringas) - 2));
+echo '<br><br>';
+?>
+
+
+<h2 style="color:#EFA00B";>---------4 Uzduotis---------</h2>
+<?php
+for ($i = 0; $i <= 30; $i++) {
+    for($j = 0; $j <= 30; $j++) {
+        echo "<span style='display: inline-block; height: 10px; color: #D65108; width: 10px;'>*</span>";
+    }
+    echo '<br>';
+}
+?>
+
+<h2 style="color:#0267C1";>---------5 Uzduotis---------</h2>
+<?php
+$length = 30;
+for ($i = 0; $i <= $length; $i++) {
+    for($j = 0; $j <= $length; $j++) {
+        if ($i ===  $j || ($i + $j) === $length) {
+            echo "<span style='display: inline-block; height: 10px; width: 10px; color:#EFA00B; '>*</span>";
+        } else {
+            echo "<span style='display: inline-block; height: 10px; width: 10px;'>*</span>";
+        }
+    }
+    echo '<br>';
+}
+?>
+
+
+
+<h2 style="color:#0075C4";>---------6 Uzduotis---------</h2>
+<?php
+?>
+
+<h2 style="color:#EFA00B";>---------7 Uzduotis---------</h2>
+<?php
+?>
+
+
+<h2 style="color:#0267C1";>---------8 Uzduotis---------</h2>
+<?php
+?>
+
+
+
+<h2 style="color:#0075C4";>---------9 Uzduotis---------</h2>
+<?php
+?>
+
+<h2 style="color:#EFA00B";>---------10 Uzduotis---------</h2>
+<?php
 ?>
--- reg.php
+++ reg.php
@@ -19,3 +19,38 @@ echo '<br>';
 $str = "Visit Microsoft!";
 $pattern = "/microsoft/i"; //i is a modifier that makes the search case-insensitive.
 echo preg_replace($pattern, "W3Schools", $str);
+?>
+<?php
+
+$animals = ['Antelope' => 'Snorts',
+'Bat' => 'Screeches',
+'Bear' => 'Roar',
+'Dolphin' => 'Clicks',
+'Goose' => 'Honks',
+'Vulture' => 'Screams'];
+
+foreach ($animals as $name => $sound) {
+    // echo $name. ' '.$sound.'<br>';
+    echo $animals[$name].'<br />';
+}
+
+echo '<span style="color:pink;font-size: 40px;">-------------</span>' . '<br>';
+
+$numbers = [1, 2, 3, 4, 5];
+print_r($numbers);
+foreach ($numbers as &$number) {
+    $number = $number *2;
+}
+echo '<br>';
+print_r($numbers);
+?>
+
+
+<?php
+echo '<br>';
+$colors = array("red", "green", "blue", "yellow");
+
+foreach ($colors as $value) {
+  echo "$value <br>";
+}
+?>
